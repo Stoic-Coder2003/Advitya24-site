80e167d28516c37d463fa0622bd797b9f53eb337
Stoic-Coder2003 <shubhmehrotra.personal@gmail.com>
2024-02-01 09:34:43 +0530

    created signin and singup pages

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ParticleRing1 from './components/Signup/ParticleRing1';
+import ParticleRing from './components/Signin/ParticleRing';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<ParticleRing />} />
+          <Route path="/signup" element={<ParticleRing1 />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
