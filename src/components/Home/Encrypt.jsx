0f0fb0690c05529fe697ddf47d6a3412f125bc38
Stoic-Coder2003 <shubhmehrotra.personal@gmail.com>
2024-02-01 15:07:14 +0530

    created home component and 2 sections

---
+++ src/components/Home/Encrypt.jsx
@@ -0,0 +1,91 @@
+import { useRef, useState } from "react";
+import { FiLock } from "react-icons/fi";
+import { motion } from "framer-motion";
+
+const Encrypt = () => {
+  return (
+    <div>
+      <EncryptButton />
+    </div>
+  );
+};
+
+const TARGET_TEXT = "More About us";
+const CYCLES_PER_LETTER = 2;
+const SHUFFLE_TIME = 50;
+
+const CHARS = "!@#$%^&*():{};|,.<>/?";
+
+const EncryptButton = () => {
+  const intervalRef = useRef(null);
+
+  const [text, setText] = useState(TARGET_TEXT);
+
+  const scramble = () => {
+    let pos = 0;
+
+    intervalRef.current = setInterval(() => {
+      const scrambled = TARGET_TEXT.split("")
+        .map((char, index) => {
+          if (pos / CYCLES_PER_LETTER > index) {
+            return char;
+          }
+
+          const randomCharIndex = Math.floor(Math.random() * CHARS.length);
+          const randomChar = CHARS[randomCharIndex];
+
+          return randomChar;
+        })
+        .join("");
+
+      setText(scrambled);
+      pos++;
+
+      if (pos >= TARGET_TEXT.length * CYCLES_PER_LETTER) {
+        stopScramble();
+      }
+    }, SHUFFLE_TIME);
+  };
+
+  const stopScramble = () => {
+    clearInterval(intervalRef.current || undefined);
+
+    setText(TARGET_TEXT);
+  };
+
+  return (
+    <motion.button
+      whileHover={{
+        scale: 1.025,
+      }}
+      whileTap={{
+        scale: 0.975,
+      }}
+      onMouseEnter={scramble}
+      onMouseLeave={stopScramble}
+      className="group relative overflow-hidden rounded-lg border-[1px] border-slate-500 bg-[#0B0311] px-4 py-2 font-mono font-medium uppercase text-white transition-colors hover:text-slate-300"
+    >
+      <div className="relative z-10 flex items-center gap-2">
+        <FiLock />
+        <span>{text}</span>
+      </div>
+      <motion.span
+        initial={{
+          y: "100%",
+        }}
+        animate={{
+          y: "-100%",
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 1,
+          ease: "linear",
+        }}
+        className="duration-300 absolute inset-0 z-0 scale-125 bg-gradient-to-t from-slate-700/0 from-40% via-slate-700/100 to-slate-700/0 to-60% opacity-0 transition-opacity group-hover:opacity-100"
+      />
+    </motion.button>
+  );
+};
+
+export default Encrypt;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className='flex justify-between items-center mx-5 text-[Poppins] z-10'>
             <img src="images\advityaLogosmall.png" alt="logo" />
-            <div className='flex items-center text-md font-regular text-black gap-12'>
+            <div className='flex items-center text-md font-regular text-white gap-14 pl-4 font-normal'>
                 <NavLink className="navs hover:text-slate-400 hover:border-b-2 hover:border-slate-400 transition duration-300 ease-in-out" to="/">Home</NavLink>
                 <NavLink className="navs hover:text-slate-400 hover:border-b-2 hover:border-slate-400 transition duration-300 ease-in-out" to="/">Events</NavLink>
                 <NavLink className="navs hover:text-slate-400 hover:border-b-2 hover:border-slate-400 transition duration-300 ease-in-out" to="/">About us</NavLink>
@@ -13,10 +13,10 @@ function Navbar() {
                 <NavLink className="navs hover:text-slate-400 hover:border-b-2 hover:border-slate-400 transition duration-300 ease-in-out" to="/">Helpdesk</NavLink>
             </div>
             <div className='flex justify-between items-center gap-5 pr-3'>
-                <button className='px-8 py-3 bg-black rounded-2xl justify-center items-center gap-2.5 flex text-white text-lg font-normal font-[Poppins] hover:bg-[#212121]/90 transition duration-300 ease-in-out'>
-                    Login</button>
-                <button className="shadow-sm hover:bg-white/90 transition duration-300 ease-in-out px-5 py-4 bg-white rounded-2xl justify-center items-center gap-2.5 flex text-black text-lg font-normal font-['Poppins']">
-                    Register</button>
+                <NavLink to="/signin" className='px-8 py-3 bg-black rounded-2xl justify-center items-center gap-2.5 flex text-white text-md font-normal font-[Poppins] hover:bg-[#212121]/90 transition duration-300 ease-in-out'>
+                    Login</NavLink>
+                <NavLink to="/signup" className="shadow-sm hover:bg-white/90 transition duration-300 ease-in-out px-5 py-3 bg-white rounded-2xl justify-center items-center gap-2.5 flex text-black text-md font-normal font-['Poppins']">
+                    Register</NavLink>
             </div>
         </nav>
     )
